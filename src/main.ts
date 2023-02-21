5ad952cb580f97fd1f61edfcdd57fa0fb36cea8c
ariel <ariel.wm1@gmail.com>
2023-02-20 22:28:12 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
-import { ValidationPipe } from '@nestjs/common'
+import { Logger, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const logger = new Logger('Bootstrap')
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   )
-  await app.listen(3000)
+  await app.listen(process.env.PORT)
+  logger.log(`Run in port ${process.env.PORT}`)
 }
 bootstrap()
