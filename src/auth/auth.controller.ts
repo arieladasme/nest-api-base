e27725e6ac7f866f227e9942537316941ae63b9b
ariel <ariel.wm1@gmail.com>
2023-02-27 15:47:57 -0300

    login user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { Controller, Post, Body, Patch, Param, Delete } from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { LoginUserDto } from './dto'
 import { CreateUserDto } from './dto/create-user.dto'
-import { UpdateAuthDto } from './dto/update-auth.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
 
 @Controller('auth')
 export class AuthController {
@@ -12,19 +13,14 @@ export class AuthController {
     return this.authService.create(createUserDto)
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll()
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id)
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto)
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto)
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.authService.update(+id, updateUserDto)
   }
 
   @Delete(':id')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,16 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common'
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import * as bcrypt from 'bcrypt'
 import { CreateUserDto } from './dto/create-user.dto'
-import { UpdateAuthDto } from './dto/update-auth.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
 import { User } from './entities/user.entity'
+import { LoginUserDto } from './dto'
 
 @Injectable()
 export class AuthService {
@@ -32,15 +38,21 @@ export class AuthService {
     return true
   }
 
-  findAll() {
-    return `This action returns all auth`
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    })
+
+    if (!user || !bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Credentials are not valid')
+
+    return user // TODO: return jwt
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
+  update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} auth`
   }
 
